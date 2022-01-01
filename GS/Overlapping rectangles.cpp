bd314db802131615be6b6e9b3b4393a92be09d47
lakshaygarg128 <lakshaygarg128@gmail.com>
2022-01-01 14:00:40 +0530

    Q2

---
+++ GS/Overlapping rectangles.cpp
@@ -0,0 +1,12 @@
+
+class Solution {
+  public:
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        // Taking the cases when the rectange will not overlap
+        if(L1[0]>R2[0] || L2[0]>R1[0])
+            return 0;
+        if(L1[1]<R2[1] || L2[1]<R1[1]) return 0;
+        
+        return 1;
+    }
+};
